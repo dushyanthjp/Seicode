5ab1662240c648367d6351cbd4cfde61b603b57b
DushyJacks <dushyanthjp2@gmail.com>
2026-07-15 00:44:18 +0530

    Minor Changes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,37 @@
-import React from 'react';
-import { Waitlist } from './components/Waitlist';
+import { useMemo, useState } from 'react';
+import { Landing } from './components/Landing';
+import { IDE } from './components/IDE';
+import { Documentation } from './components/Documentation';
+import { About } from './components/About';
+
+type View = 'landing' | 'ide' | 'docs' | 'about';
+
+const initialCode = `// Welcome to Sei
+Vai msg = "Vanakkam"
+Kaami msg`;
 
 export default function App() {
-  return <Waitlist />;
+  const [view, setView] = useState<View>('landing');
+
+  const currentView = useMemo(() => {
+    switch (view) {
+      case 'ide':
+        return <IDE initialCode={initialCode} onBack={() => setView('landing')} />;
+      case 'docs':
+        return <Documentation onBack={() => setView('landing')} />;
+      case 'about':
+        return <About onBack={() => setView('landing')} />;
+      case 'landing':
+      default:
+        return (
+          <Landing
+            onStart={() => setView('ide')}
+            onOpenDocs={() => setView('docs')}
+            onOpenAbout={() => setView('about')}
+          />
+        );
+    }
+  }, [view]);
+
+  return currentView;
 }
